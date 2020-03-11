96b4d237e3d6deb3f106da3467d384cfc1a52ad4
brunocontrerasmoreira <bcontreras@ebi.ac.uk>
2020-03-11 11:22:30 +0000

    now checks input/output filenames

--- src/main/java/dnaformatter/PdfMain.java
+++ src/main/java/dnaformatter/PdfMain.java
@@ -9,20 +9,30 @@ public class PdfMain {
   public static void main(String[] args) throws IOException, DocumentException {
     String inputFile;
     String outputFile;
+
     if(args.length == 0) {
-      inputFile = System.getenv("HTML");
+      inputFile = System.getenv("HTML"); 
       outputFile = System.getenv("PDF");
     }
     else {
       inputFile = args[0];
       outputFile = args[1];
     }
-    String url = new File(inputFile).toURI().toURL().toString();
-    OutputStream os = new FileOutputStream(outputFile);
-    ITextRenderer renderer = new ITextRenderer();
-    renderer.setDocument(url);
-    renderer.layout();
-    renderer.createPDF(os);
-    os.close();
+
+    if(inputFile == null || inputFile.length() == 0) {
+      System.out.println("# error: input filename is empty");
+    }
+    else if(outputFile == null || outputFile.length() == 0) {
+      System.out.println("# error: output filename is empty");
+    }
+    else {
+      String url = new File(inputFile).toURI().toURL().toString();
+      OutputStream os = new FileOutputStream(outputFile);
+      ITextRenderer renderer = new ITextRenderer();
+      renderer.setDocument(url);
+      renderer.layout();
+      renderer.createPDF(os);
+      os.close();
+    }
   }
 }
