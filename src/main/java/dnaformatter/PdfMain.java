1942aef1fd036cac177ce435edbe1a45b3cb7068
brunocontrerasmoreira <bcontreras@ebi.ac.uk>
2020-03-11 11:47:26 +0000

    now checks input/output filenames

--- src/main/java/dnaformatter/PdfMain.java
+++ src/main/java/dnaformatter/PdfMain.java
@@ -10,6 +10,7 @@ public class PdfMain {
     String inputFile;
     String outputFile;
 
+    // pick filenames from environment, note gradle does not take the same environment by default
     if(args.length == 0) {
       inputFile = System.getenv("HTML"); 
       outputFile = System.getenv("PDF");
